5bed395bb0587c2f39d5a5826a0a689865f6bd43
sjoshi01 <sayali.m.joshi@gmail.com>
2017-01-18 00:49:46 -0500

    Update ReverseKGroupLinkedList.java

--- src/LeetCodeLinkedList/ReverseKGroupLinkedList.java
+++ src/LeetCodeLinkedList/ReverseKGroupLinkedList.java
@@ -8,7 +8,7 @@ public class ReverseKGroupLinkedList {
         dummy.next = head;
         ListNode prev = dummy;
         ListNode nextNode = dummy;
-        while(true)
+        while(true)//Need to repeate this loop till the end of linkedList
         {
             int count =0;
             while(nextNode != null && count < k)
@@ -18,15 +18,15 @@ public class ReverseKGroupLinkedList {
             }
             if(nextNode == null)
               break;
-            ListNode nextCycle = prev.next;
-            while(prev.next != nextNode)
+            ListNode nextCycle = prev.next; // when K group nodes are reversed, prev.next will become starting node for next group
+            while(prev.next != nextNode)//loop till current group last element doesn't updated to current prev's next node
             {
                 ListNode temp = prev.next;
                 prev.next = temp.next;
                 temp.next = nextNode.next;
                 nextNode.next = temp;
             }
-            nextNode = nextCycle;
+            nextNode = nextCycle;//for next group prev node will be current prev's next which updated to end of current group after reverse
             prev = nextCycle;
         }
         return dummy.next;
